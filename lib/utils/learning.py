2f470db6b3a087d1c75f464f1f9208f75b6cb23f
dajiaohuang <mikewushuwen@outlook.com>
2024-05-14 22:48:51 +0800

    add bi

--- lib/utils/learning.py
+++ lib/utils/learning.py
@@ -92,7 +92,9 @@ def load_backbone(args):
         from lib.model.STmamba import STmamba
         model_backbone = STmamba(dim_in=3, dim_out=3,dim_feat=args.dim_feat, dim_rep= args.dim_rep, 
                                  depth= args.depth, num_joints= args.num_joints, maxlen= args.maxlen,
-                                 norm_layer=partial(nn.LayerNorm, eps=1e-6))
+                                 norm_layer=partial(nn.LayerNorm, eps=1e-6),
+                                 if_bidirectional = args.if_bidirectional
+                                 )
     # elif args.backbone=='TCN':
     #     from lib.model.model_tcn import PoseTCN
     #     model_backbone = PoseTCN()
